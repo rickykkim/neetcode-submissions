7afae06f120325a566b1c361ff932208be38c9d7
Ricky Kim <rickay1129@gmail.com>
2026-09-22 14:00:35 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        cache = set()
+        for num in nums:
+            if num in cache:
+                return True
+            cache.add(num)
+        return False
