c429e3001c44a4a512aa54ae456fd95c99eafca8
Ricky Kim <rickay1129@gmail.com>
2026-09-23 12:03:52 -0400

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        cache = set()
+        cache_r = set()
+        for num in nums:
+            if num in cache:
+                cache_r.add(num)
+            else:
+                cache.add(num)
+        
+        temp = cache - cache_r
+        return next(iter(temp))
